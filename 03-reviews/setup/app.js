a1063a798662e474a3e08222f9b0e68da3621db7
Atomic-Coco <dkline.reach@gmail.com>
2025-04-20 16:50:52 -0500

    completed project 03-reviews. turned off the scary copilot auto-complete

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -29,56 +29,55 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
-// select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+// get items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
 
-// set starting item
-let currentItem = 0;
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn"); 
 
-// load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-});
+// set initial review
+ let currentItem = 3;
 
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-}
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
+ //load initial review
+  window.addEventListener("DOMContentLoaded", function(){
+    showReview(currentItem); 
+  });   
 
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
+  //show review based on item
+   function showReview(person) {
+    const item = reviews[person];   
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+   }
+
+   // show next review
+   nextBtn.addEventListener("click", function(){
+    currentItem++;
+    if(currentItem > reviews.length -1){
+      currentItem = 0;
+    }
+    showReview(currentItem);
+   });
+
+   // show previous review
+   prevBtn.addEventListener("click", function(){
+    currentItem--;
+    if(currentItem < 0){
+      currentItem = reviews.length - 1;
+    }
+    showReview(currentItem);
+   });
+
+   // show random review
+   randomBtn.addEventListener("click", function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    console.log(currentItem);
+    showReview(currentItem);
+   });
